57b22badadb67a539d736c3836de66b5cf83195d
KovshovaMariya <mash.kovshova@yandex.ru>
2020-06-17 14:07:21 +0300

    Initial commit

---
+++ src/com/company/Nine.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import javax.script.ScriptEngine;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
+import java.util.Vector;
+
+public class Nine {
+    public static void main(String[] args) {
+        try {
+            System.out.println(formula("6 * 4 = 24"));
+        } catch (ScriptException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static boolean formula(String str) throws ScriptException {
+        ScriptEngineManager mgr = new ScriptEngineManager();
+        ScriptEngine engine = mgr.getEngineByName("JavaScript");
+        String[] expressions = str.split("=");
+        Vector<Integer> results = new Vector<Integer>();
+        for (String expression : expressions) {
+            results.add((Integer) engine.eval(expression));
+        }
+        int check = results.get(0);
+        for (Integer value : results) {
+            if (value != check)
+                return false;
+        }
+        return true;
+    }
+}
